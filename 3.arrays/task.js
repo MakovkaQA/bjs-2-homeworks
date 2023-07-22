7d7274a0a10e2b153e6395f5a88b28283cddbbdf
Anastasiya <makeeva.anastasya89@gmail.com>
2023-07-23 00:00:42 +0300

    arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,19 @@
 function compareArrays(arr1, arr2) {
-  
+  if(arr1.length === arr2.length) {
+  return arr1.every((element, index) => element === arr2[index]); 
+  } else {
+    return false;
+  }
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  if(users.length === 0) {
+    return 0;
+  };
+  const selectionByGender = users.filter(user => user.gender === gender);
+  if(selectionByGender.length ===0) {
+    return 0;
+  };
+  let result = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, age) => {return acc + age});
+  return result / selectionByGender.length;  
 }
